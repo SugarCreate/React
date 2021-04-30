6d34216c099256e4ee8c990d7c46cef02d2d42b0
SuagrCreate-TencentECS <touchlover@foxmail.com>
2021-04-30 15:45:32 +0800

    game init

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,9 +2,69 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Square extends React.Component {
+  render() {
+    return(
+      <button className="square">
+        {/* \TODO */}
+      </button>
+    );
+  }
+}
 
 
+class Board extends React.Component{
+  renderSquare(i) {
+    return <Square />;
+  }
 
+  render() {
+    const status = 'Next play: X'
+
+    return(
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class Game extends React.Component {
+  render() {
+    return(
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{ /* status */ }</div>
+          <ol>{ /* \TODO */ }</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+)
 
 // // 从Calculator 组件中抽离出 Temperature组件，然后为其添加一个新的scale prop ,它可以是 'c' 或者 "f"
 
